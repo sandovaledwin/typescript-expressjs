0671eda5cb7e38bd1cb34546618b916881667ad9
Carlos Edwin Sandoval Diaz <sandovaledwin@hotmail.com>
2017-12-03 16:53:47 -0600

    Was added the support for request-promise library

--- src/Tests/User.spec.ts
+++ src/Tests/User.spec.ts
@@ -1,7 +1,30 @@
 import {} from 'jasmine';
+import * as request from 'request-promise';
 
 describe( "Simple Validation", () => {
     it( "Expect TRUE", () => {
-        expect( true ).toBe( false );
+        expect( true ).toBe( true );
+    });
+});
+
+describe( "GET /api/people/1/", () => {
+
+    it( "Expect the Luke Skywalker JSON object", ( done ) => {
+        
+        let options = {
+            uri: 'https://swapi.co/api/people/1/',
+            json: true 
+        };
+
+        request( options )
+        .then( function ( response ) {
+            expect( response.name ).toContain( "Luke Skywalker" );
+            done();
+        })
+        .catch( function ( err ) {
+            console.log( err );
+            done();
+        });                
+
     });
 });
