a59ce83b1bbaed3c5ffc45f648677a0ab0a456ac
Carlos Edwin Sandoval Diaz <sandovaledwin@hotmail.com>
2017-12-08 15:36:58 -0600

    It was added enable CORS - OPTIONS response

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,14 @@ app.use( logger( "dev", {
   }) 
 );
 
+//enable CORS: OPTIONS response
+app.options( "/*", function( req : any, res : any, next : any ){
+  res.header( 'Access-Control-Allow-Origin',  '*' );
+  res.header( 'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS' );
+  res.header( 'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With' );
+  res.sendStatus( 200 );
+});
+
 //use json form parser middlware
 app.use( bodyParser.json() );
 
